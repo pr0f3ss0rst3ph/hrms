11af665946199430dffba40f0ce4d12e20cb8c1a
Stephon Fraser <43830615+pr0f3ss0rst3ph@users.noreply.github.com>
2021-07-11 23:31:29 -0400

    Specified blocks of code to be returned based on button clicked.

--- src/components/Smallmenu.js
+++ src/components/Smallmenu.js
@@ -1,4 +1,5 @@
 import more from '../images/more.png'
+import Profilemenu from './Profilemenu'
 
 const Smallmenu = ({ type }) => {
 
@@ -10,13 +11,7 @@ const Smallmenu = ({ type }) => {
     if(type  === 'messages'){
         menuItems.title = 'Messages'
         menuItems.content = 'No new messages.'
-    }
-    else if (type === 'notifications'){
-        menuItems.title = 'Notifications'
-        menuItems.content = 'No new notifications'
-    }
-    
-    return (
+        return (
         <div className='menubox'>
             <div className='menu-box-top'>
                 <div className='menu-title'>
@@ -36,6 +31,39 @@ const Smallmenu = ({ type }) => {
             </div>
         </div>
     )
+    }
+    else if (type === 'notifications'){
+        menuItems.title = 'Notifications'
+        menuItems.content = 'No new notifications'
+        return (
+        <div className='menubox'>
+            <div className='menu-box-top'>
+                <div className='menu-title'>
+                    { menuItems.title }
+                </div>
+                <div className='menu-more'>
+                    <img alt='more' src={ more } id='more' />
+                </div>
+            </div>
+            <div className='menu-body'>
+                <div className='menu-details'>
+                    { menuItems.content }
+                </div>
+                <div className='menu-all-details'>
+                    View All
+                </div>
+            </div>
+        </div>
+    )
+    }
+    else if (type === 'profile'){
+        menuItems.title = 'Hello, User!'
+        return (
+        <>
+        <Profilemenu />
+        </>
+    )
+    }
 }
 
 export default Smallmenu
